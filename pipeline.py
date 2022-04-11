968b1da8f2936cc69e012d2b0ce040b696072840
tjems6498 <tjems6498@gmail.com>
2022-04-11 15:23:16 +0900

    d

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,30 @@
 import kfp
 from kfp import dsl
 from kfp import onprem
+# TODO: dataloader num_workers 값을 주었을때 shm 문제해결
+
+def preprocess_op(pvc_name, volume_name, volume_mount_path):
+
+    return dsl.ContainerOp(
+        name='Preprocess Data',
+        image='tjems6498/kfp-yolov5-preprocess:v0.1',
+        arguments=['--data-path', volume_mount_path,
+                   '--data-path', volume_mount_path + '/classname.txt'],
+    ).apply(onprem.mount_pvc(pvc_name, volume_name=volume_name, volume_mount_path=volume_mount_path))
 
 
+@dsl.pipeline(
+    name='Surface Crack Pipeline',
+    description=''
+)
+def surface_pipeline():
+    pvc_name = "workspace-yolov5"
+    volume_name = 'pipeline'
+    volume_mount_path = '/home/jeff'
+
+
+    _preprocess_op = preprocess_op(pvc_name, volume_name, volume_mount_path)
+
+
+if __name__ == '__main__':
+    kfp.compiler.Compiler().compile(surface_pipeline, './kfp.yaml')
